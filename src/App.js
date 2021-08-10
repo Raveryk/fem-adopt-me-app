260e715dbdbd1b85b5742636b3a9cec0a080af70
Raveryk <raveryk@gmail.com>
2021-08-10 10:18:18 -0500

    Further project set up.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-const Pet = () => {
+// Using props to pass from parent (App)
+// into child component (Pet)
+// Can only go down from parent to child component
+const Pet = (props) => {
   return React.createElement('div', {}, [
-    React.createElement('h2', {}, 'Luna'),
-    React.createElement('h3', {}, 'Dog'),
-    React.createElement('h3', {}, 'Havanese'),
+    React.createElement('h2', {}, props.name),
+    React.createElement('h3', {}, props.animal),
+    React.createElement('h3', {}, props.breed),
   ]);
 };
 
@@ -12,9 +15,22 @@ const Pet = () => {
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    //creating new instances of the Pet component
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Sudo',
+      animal: 'Dog',
+      breed: 'Wheaten Terrier',
+    }),
   ]);
 };
 
