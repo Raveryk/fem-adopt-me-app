1a2b51a7e7553e072aaeec40423e88ef03dcdde5
Raveryk <raveryk@gmail.com>
2021-08-10 13:07:34 -0500

    Update components to contain JSX.

--- src/Pet.js
+++ src/Pet.js
@@ -3,12 +3,23 @@ import React from "react";
 // Using props to pass from parent (App)
 // into child component (Pet)
 // Can only go down from parent to child component
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
+
+// Same component as above but written with JSX
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
 };
 
 export default Pet;
